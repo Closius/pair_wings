0d5eed811e92931f2de500e2a8a6e116eb8cd5f0
mdkk4v <anton.kavalerov@siemens.com>
2024-09-15 17:49:38 +0200

    Clean architecture

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import os
 import json5
 import logging
 
-from pybit.unified_trading import HTTP
+from lib import data_show, data_collector
 
-import data_collector
-import data_show
+from lib.stocks.stock_bybit import StockBybit
 
 
 def main():
@@ -19,50 +18,37 @@ def main():
     logging.getLogger().addHandler(logging.StreamHandler())
     log = logging.getLogger(__name__)
 
-    account_name = "pair_wings_demo"
-    with open("api_secret.json") as f:
-        api_secrets = json5.load(f)
-    with open("settings.json") as f:
-        settings = json5.load(f)
-
-    log.info("Current settings:")
-    log.info(json5.dumps(settings, indent=4))
-
-    log.info(f"Connecting to: '{account_name} ...'")
-    # You can create an authenticated or unauthenticated HTTP session.
-    # You can skip authentication by not passing any value for the key and secret.
-    session = HTTP(
-        api_key=api_secrets["accounts"][account_name]["API_KEY"],
-        api_secret=api_secrets["accounts"][account_name]["API_SECRET"],
-        demo=True,
-    )
-
     log.info("==========================")
-    log.info("Current settings:")
+    log.info("Choose your destiny:")
     log.info("1 - Collect tickers")
     log.info("2 - Collect candles")
     log.info("3 - Show data")
     log.info("4 - Quit")
-    r = int(input())
+    r = input()
     log.info(r)
+    # stock = StockBybit(account_name="pair_wings_demo",
+    #                    api_secrets_file="api_secret.json",
+    #                    settings_file="settings.json")
+    stock = StockBybit()
     db_filepath = "main.db"
-    if r == 1:
+    if r == "1":
         data_collector.collect_stream_tickers(
+            stock=stock,
             filepath=db_filepath,
             pair="BTCUSDT",
             recreate=True)
 
-    elif r == 2:
+    elif r == "2":
         data_collector.collect_history_candles(
+            stock=stock,
             filepath=db_filepath,
             recreate=True,
             pair="BTCUSDT",
             interval="5",
             start='14.09.2024 13:00:00,00')
-    elif r == 3:
+    elif r == "3":
         data_show.draw_candles(db_filepath, pair="BTCUSDT")
-    elif r == 4:
-        pass
+
 
 if __name__ == "__main__":
     main()
